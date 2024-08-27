02a27c02e61706194030e328169b729564dc8777
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-08-27 14:02:23 +0700

    feat(67): use the sort return

--- admin-panel-dashboard/src/App.tsx
+++ admin-panel-dashboard/src/App.tsx
@@ -1,16 +1,14 @@
 // Layouts
 import PageLayout from '@layouts/PageLayout';
 
-const App = () => {
-  return (
-    <PageLayout>
-      <header className="App-header">
-        <h1 className="text-3xl text-center font-bold underline text-red-600">
-          Simple Storybook, Jest, TypeScript, Eslint, Husky, Vite and TailwindCSS Sample
-        </h1>
-      </header>
-    </PageLayout>
-  );
-};
+const App = () => (
+  <PageLayout>
+    <header className="App-header">
+      <h1 className="text-3xl text-center font-bold underline text-red-600">
+        Simple Storybook, Jest, TypeScript, Eslint, Husky, Vite and TailwindCSS Sample
+      </h1>
+    </header>
+  </PageLayout>
+);
 
 export default App
