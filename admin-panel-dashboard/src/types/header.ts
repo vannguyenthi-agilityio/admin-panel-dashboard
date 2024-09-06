edb97426457d05bd2a7839b44808121463cf6ea6
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-09-06 08:36:42 +0700

    feat(69): update props for header component

--- admin-panel-dashboard/src/types/header.ts
+++ admin-panel-dashboard/src/types/header.ts
@@ -1,11 +1,7 @@
-import { VariantProps } from "class-variance-authority";
-
-import { headerStyles } from '@/components/Header/style';
-
 import { IUser } from './common';
 
-export interface IHeader
-  extends Omit<VariantProps<typeof headerStyles>, "">  {
+export interface IHeader {
   className?: string;
   user?: IUser;
+  size?: 'xs' | 'sm' | 'md' | 'lg';
 }
