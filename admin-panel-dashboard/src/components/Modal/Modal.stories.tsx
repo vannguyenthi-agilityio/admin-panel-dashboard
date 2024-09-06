349b26a5ff2470fa099521b9fdb86e5d90f8da1c
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-09-06 16:48:57 +0700

    feat(72): implement component modal

---
+++ admin-panel-dashboard/src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,31 @@
+// Libs
+import type { Meta, StoryObj } from "@storybook/react";
+
+// Components
+import Modal from ".";
+
+const meta: Meta<typeof Modal> = {
+  title: "Components/Modal",
+  tags: ["autodocs"],
+  component: Modal,
+  argTypes: {
+    open: { description: "open modal type is boolean"},
+    onClose: { description: "funct close modal type is void"},
+    children: { description: "body modal type is ReactNode"},
+    showIconClose: { description: "check to show close icon type is boolean"},
+    labelButton: { description: "label text to show button in footer of modal type is string"},
+    title: { description: "title of modal type is string"},
+    className: { description: "custom class for modal type is string"},
+    size: { description: "custom size padding, text size for modal with option type: 'xs' | 'sm' | 'md' | 'lg'"},
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <p className="py-4"> Modal body text goes here.</p>
+  },
+};
